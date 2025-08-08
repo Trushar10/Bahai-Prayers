1db11492e551eba69d53f1a0bd25544410dae274
Trushar10 <trushargavit@gmail.com>
2025-08-08 19:26:38 +0530

    font Rasa added

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,14 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { Rasa } from 'next/font/google'
+
+const rasa = Rasa({
+  subsets: ['latin'],
+  weight: ['400', '600', '700'], // Add weights you need
+  variable: '--font-rasa',       // Use CSS variable
+  display: 'swap',
+})
+
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
