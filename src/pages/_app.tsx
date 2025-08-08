4c685316eb142f4e4656c574a19c924d19df149a
Trushar10 <trushargavit@gmail.com>
2025-08-08 19:34:07 +0530

    minor fix

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,18 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import '@/styles/globals.css'
+import type { AppProps } from 'next/app'
 import { Rasa } from 'next/font/google'
 
 const rasa = Rasa({
   subsets: ['latin'],
-  weight: ['400', '600', '700'], // Add weights you need
-  variable: '--font-rasa',       // Use CSS variable
+  weight: ['400', '700'],
+  variable: '--font-rasa',
   display: 'swap',
 })
 
-
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <main className={rasa.variable}>
+      <Component {...pageProps} />
+    </main>
+  )
 }
