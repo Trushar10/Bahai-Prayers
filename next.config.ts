f3b921cae4f82e39df68e06c92810c7415e65e75
Trushar10 <trushargavit@gmail.com>
2025-08-09 14:02:01 +0530

    prechache fixes

--- next.config.ts
+++ next.config.ts
@@ -1,21 +1,11 @@
-// next.config.js
 const withPWA = require('next-pwa')({
   dest: 'public',
   register: true,
   skipWaiting: true,
+  swSrc: 'public/sw.js',
   disable: process.env.NODE_ENV === 'development',
-  runtimeCaching: [
-    {
-      urlPattern: /^https?.*/,
-      handler: 'NetworkFirst',
-      options: {
-        cacheName: 'html-cache',
-        expiration: { maxAgeSeconds: 60 * 60 * 24 * 365 },
-      },
-    },
-  ],
-})
+});
 
 module.exports = withPWA({
   reactStrictMode: true,
-})
+});
