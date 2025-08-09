1b0aabff9f9c4aca85563b72ec571897528a4154
Trushar10 <trushargavit@gmail.com>
2025-08-09 10:43:53 +0530

    Speedinsight vercel added

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import '@/styles/prayers.css'
 import type { AppProps } from 'next/app'
 import { Rasa } from 'next/font/google'
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const rasa = Rasa({
   subsets: ['latin'],
@@ -13,6 +14,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <main className={rasa.variable}>
       <Component {...pageProps} />
+      <SpeedInsights />
     </main>
   )
 }
